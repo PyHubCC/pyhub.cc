41b8e0d30c0f245608e7c5caac9d59762367757b
rainyear <sniky.hack@gmail.com>
2016-04-14 21:29:23 +0800

    home page

---
+++ pycc.py
@@ -0,0 +1,37 @@
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+from tornado.options import define, options
+import os.path
+
+_ROOT = os.path.dirname(__file__)
+_ROOT_JOIN = lambda sub: os.path.join(_ROOT, sub)
+
+define("port", default=8080, type=int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [(r'/', HomeHandler)]
+        settings = dict(
+            template_path=_ROOT_JOIN('templates'),
+            debug=True
+        )
+        super(Application, self).__init__(handlers=handlers, **settings)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    pass
+
+class HomeHandler(BaseHandler):
+    def get(self):
+        self.render("home.html", title='PyHub.cc')
+    def post(self, *args, **kwargs):
+        self.redirect("/")
+
+def main():
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == '__main__':
+    main()
