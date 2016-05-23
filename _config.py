b7c790fa0ddee3180097b19aea9334e7f83f82ea
rainyear <sniky.hack@gmail.com>
2016-05-23 13:03:59 +0800

    config file into private repo

---
+++ _config.py
@@ -0,0 +1,30 @@
+# SHOULD BE IGNORED BY GIT
+# AND SHOULD NOT BE PUBLIC
+
+class Env:
+    env='dev'
+
+    DB_HOST='127.0.0.1'
+    DB_NAME='pyhub'
+
+    COL_USER = 'users'
+    COL_LINK = 'links'
+    COL_EVENT = 'events'
+    COL_MSG  = 'msg'
+    COL_FAV = 'fav'
+    COL_COMMENT = 'comment'
+    COL_TOPIC_META = 'topic_meta'
+    COL_TOPIC = 'topic'
+    COL_ACCOUNT = 'account'
+    COL_ACCOUNT_META = 'account_meta'
+
+    POST_KEY = 'pyhub_yushneng'
+
+    GITHUB_ID = '195687a3fec548b61fa3'
+    GITHUB_SEC = 'fc97389865984797c320e0a963a3d96b3440b163'
+    GITHUB_REDIRECT = 'https://pyhub.cc/oauth/github'
+
+    COOKIE_SEC = '#@$%AHGAJ%*$!JAA@#'
+
+    ADMIN_USER = 'rainyear'
+    AVATAR = '/static/default_avatar.png'
